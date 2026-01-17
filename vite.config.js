03a78e04c10ff5f9c7b55cbadb8c9eff96d3d0a5
Vellorey <velloreystore@gmail.com>
2026-01-17 23:19:00 +0500

    netify gl

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // this tells Vite to put production files in 'build' instead of 'dist'
+  },
 });
